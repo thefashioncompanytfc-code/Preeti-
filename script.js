e8660a01c0edca997ebd89c225854c821339079a
thefashioncompanytfc-code <thefashioncompany.tfc@gmail.com>
2026-09-12 12:38:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Birthday Engine V1 started 🎂");
+const button = document.getElementById("celebrateBtn");
+const confettiContainer = document.getElementById("confetti-container");
+
+button.addEventListener("click", function () {
+
+    for (let i = 0; i < 60; i++) {
+
+        const confetti = document.createElement("span");
+
+        confetti.classList.add("confetti");
+
+        confetti.style.left = Math.random() * 100 + "vw";
+        confetti.style.animationDelay = Math.random() * 0.5 + "s";
+
+        confettiContainer.appendChild(confetti);
+
+        setTimeout(() => {
+            confetti.remove();
+        }, 4000);
+    }
+});
